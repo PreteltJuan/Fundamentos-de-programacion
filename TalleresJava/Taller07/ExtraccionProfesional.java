1213f08dc0450907e12bd81d4c3b26db92dc0cc8
Pretel Juan <preteltjuanoficial@gmail.com>
2021-03-16 13:49:05 -0500

    ExtraccionProfesional was modified

---
+++ TalleresJava/Taller07/ExtraccionProfesional.java
@@ -0,0 +1,35 @@
+package TalleresJava.Taller07;
+
+import java.util.Scanner;
+
+/*
+*Teniendo <div class='name'>NOMBRE</div><div class='lastname'>APELLIDO</div>
+*Cree un programa que extraiga la información de NOMBRE y APELLIDO y los muestre por pantalla concatenados
+*/
+
+public class ExtraccionProfesional{
+    public static void main(String a[]){
+        
+        Scanner in = new Scanner(System.in);
+        
+        String cadena = in.nextLine();
+        String nombreCompleto = "";
+        
+        int indexInicioNombre = cadena.indexOf("'>")+2;
+        int indexFinalNombre = cadena.indexOf("<", indexInicioNombre);
+        nombreCompleto += cadena.substring(indexInicioNombre, indexFinalNombre );
+        
+        int indexInicioApellido = cadena.indexOf("'>", indexFinalNombre) +2;
+        int indexFinalApellido = cadena.indexOf("<", indexInicioApellido);
+        nombreCompleto += cadena.substring(indexInicioApellido, indexFinalApellido );
+        
+        System.out.println(nombreCompleto);
+
+        in.close();
+
+    }
+    /*
+    *Esta forma solo funciona si solo hay dos campos a recuperar. En este caso nombre y apellido.
+    *
+    */
+}
